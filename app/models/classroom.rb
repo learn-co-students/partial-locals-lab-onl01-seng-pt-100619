efc9ac238fdca3ba306bffa26092e20e6c4859cd
rgonzalez12 <raymondalexander.gonzalez@gmail.com>
2020-03-23 20:54:39 -0600

    Done.

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -1,14 +1,3 @@
-# == Schema Information
-#
-# Table name: classrooms
-#
-#  id          :integer          not null, primary key
-#  course_name :string
-#  semester    :string
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#
-
 class Classroom < ActiveRecord::Base
   has_many :classroom_students
   has_many :students, through: :classroom_students
@@ -16,4 +5,5 @@ class Classroom < ActiveRecord::Base
   def oldest_student
     students.where("birthday is not null").order("birthday asc").first
   end
+
 end
